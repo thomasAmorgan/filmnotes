ee91a07b629946bf86663aa5346e847138b0ae85
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-14 22:25:55 -0500

    didnt add certain files, so now including them

---
+++ src/components/ViewRolls.vue
@@ -0,0 +1,52 @@
+<!-- handles the displying of all the rolls in the rolls array -->
+<template lang="html">
+
+  <div class="">
+        <!-- will emit an event to the parent component that will add a new roll -->
+        <button class="w3-bar w3-button w3-blue-gray w3-small"
+                @click="addRoll">Create Roll
+        </button>
+
+        <hr>
+
+        <!-- loops through all the rolls in the array and displays them -->
+        <app-rolls v-for="(roll, index) in rolls"
+                   v-model="rolls[index]"
+                   @deleteRoll="removeRoll">
+        </app-rolls>
+
+        <!-- TODO: add a filter option -->
+
+  </div>
+
+</template>
+
+<script>
+  import Rolls from './Rolls.vue';
+  import Shots from './Shots.vue';
+
+  export default {
+    // allows access to the array of rolls from the parent component
+    props: ["rolls"],
+
+    methods: {
+      addRoll() {
+        this.$emit("newRoll");
+      },
+      // emits the index of the roll that the user wants to delete, the parent
+      // will read in the index and then delete the correct roll
+      removeRoll(roll) {
+        var rollIndex = this.rolls.indexOf(roll);
+        this.$emit("deleteRoll", rollIndex);
+      }
+    },
+
+    components: {
+      'app-rolls': Rolls,
+      'app-shots': Shots,
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
