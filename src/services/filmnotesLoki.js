99fe25594f5df365a5782c2fbd3e3ec4fb1a0954
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-30 01:35:09 -0500

    tried to add a database using loki and IndexedAdapter but was unsucessful , may want to try sqlite or using loki with a server

---
+++ src/services/filmnotesLoki.js
@@ -0,0 +1,12 @@
+import loki from 'lokijs';
+// var LokiIndexedAdapter = require('loki-indexed-adapter.js')
+
+// const idbAdapter = new LokiIndexedAdapter('test');
+// adapter: idbAdapter,
+// const rollsDatabase = new loki('loki', {
+//   autosave: true,
+//   autosaveInterval: 1000
+// });
+// const rollCollection = rollsDatabase.addCollection('rollCollection');
+
+// export default rollCollection;
